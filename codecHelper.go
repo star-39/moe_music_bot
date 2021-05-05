7c12cb0551dc17816ac9377123424cf0c6375bf9
star-39 <star3nine@mail.ru>
2021-05-05 19:04:11 +0900

    Fix ffmpeg error on Windows

--- codecHelper.go
+++ codecHelper.go
@@ -11,6 +11,7 @@ var ffmpegPath = "ffmpeg"
 
 // opus only, it's the best, and Telegram supports it across all devices.
 func transcodeAudio(fileName string, bitrate string) (string, error) {
+	println(ffmpegPath)
 	ffErr := checkFFMpeg()
 	if ffErr != nil {
 		return "", ffErr
@@ -19,6 +20,8 @@ func transcodeAudio(fileName string, bitrate string) (string, error) {
 	tempDir := os.TempDir()
 	fileBaseName := filepath.Base(fileName)
 	outputFileName := filepath.Join(tempDir, "_temp_"+fileBaseName+".opus")
+	print(fileBaseName)
+	println(outputFileName)
 
 	cmd := exec.Command(ffmpegPath, "-i", fileName, "-c:a", "libopus",
 		"-b:a", bitrate, "-y", outputFileName)
@@ -31,7 +34,7 @@ func transcodeAudio(fileName string, bitrate string) (string, error) {
 }
 
 func checkFFMpeg() error {
-	cmd := exec.Command(ffmpegPath)
+	cmd := exec.Command(ffmpegPath, "-h")
 	err := cmd.Run()
 	if err != nil {
 		return err
